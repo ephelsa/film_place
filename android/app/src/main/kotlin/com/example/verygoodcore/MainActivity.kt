0ca4ab0a9bb886a13f7e904e3c8d2f7d438fe4a7
Leonardo <xephelsax@gmail.com>
2022-04-18 19:45:40 -0500

    Create project using `very_good` CLI

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.film_place
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, Very Good Ventures
+// https://verygood.ventures
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
+import 'package:film_place/app/app.dart';
+import 'package:film_place/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
