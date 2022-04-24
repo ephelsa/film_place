106b76ee8e7f2d8bc12dc93884b1b73ba27c9738
Leonardo <xephelsax@gmail.com>
2022-04-24 14:15:52 -0500

    Add real implementation in `GenreRepository`

--- packages/genre_repository/lib/src/genre_repository.dart
+++ packages/genre_repository/lib/src/genre_repository.dart
@@ -1,7 +1,35 @@
+import 'package:genre_repository/src/models/genre.dart';
+import 'package:the_movie_db_api/the_movie_db_api.dart';
+
 /// {@template genre_repository}
 /// Dart package which manages the genre domain
 /// {@endtemplate}
 class GenreRepository {
   /// {@macro genre_repository}
-  const GenreRepository();
+  const GenreRepository({
+    required GenreApi genreApi,
+  }) : _genreApi = genreApi;
+
+  final GenreApi _genreApi;
+
+  Future<List<Genre>> refreshMovieGenres() async {
+    final remoteResult = await _genreApi.getMovieGenres();
+    // TODO(ephelsa): Store the data locally
+
+    return remoteResult?.genres.map(Genre.fromGenreRemote).toList() ?? [];
+  }
+
+  Future<List<Genre>> getMovieGenresByIds(List<int> ids) async {
+    // TODO(ephelsa): Check local storage and return
+    final genres = await refreshMovieGenres();
+
+    return ids
+        .map((id) => genres.firstWhere((genre) => genre.id == id))
+        .toList();
+  }
+
+  Future<List<Genre>> getAllMovieGenres() async {
+    // TODO(ephelsa): Check local storage and return
+    return refreshMovieGenres();
+  }
 }
--- packages/genre_repository/lib/src/models/genre.dart
+++ packages/genre_repository/lib/src/models/genre.dart
@@ -1,10 +1,18 @@
 import 'package:equatable/equatable.dart';
+import 'package:the_movie_db_api/the_movie_db_api.dart';
 
 class Genre extends Equatable {
   const Genre({
     required this.id,
     required this.name,
   });
+
+  factory Genre.fromGenreRemote(GenreRemote remote) {
+    return Genre(
+      id: remote.id,
+      name: remote.name,
+    );
+  }
 
   final int id;
   final String name;
