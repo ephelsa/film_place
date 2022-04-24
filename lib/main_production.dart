8b57147c30b04045e58099aeeb816b518d1b8cd7
Leonardo <xephelsax@gmail.com>
2022-04-24 14:40:55 -0500

    Add changes for production and staging

--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,11 +1,29 @@
 import 'package:film_place/bootstrap.dart';
 import 'package:flutter_services_binding/flutter_services_binding.dart';
+import 'package:genre_repository/genre_repository.dart';
 import 'package:movie_repository/movie_repository.dart';
+import 'package:the_movie_db_api/the_movie_db_api.dart';
 
 void main() {
   FlutterServicesBinding.ensureInitialized();
 
+  final httpClient = TMDBClient.client(
+    'en-US',
+    'YOUR-API-KEY-HERE',
+  );
+
+  // APIs
+  final genreApi = GenreApi(httpClient);
+  final movieApi = MovieApi(httpClient);
+
+  // Repositories
+  final genreRepository = GenreRepository(genreApi: genreApi);
+  final movieRepository = MovieRepository(
+    movieApi: movieApi,
+    genreRepository: genreRepository,
+  );
+
   bootstrap(
-    movieRepository: const MovieRepository(),
+    movieRepository: movieRepository,
   );
 }
