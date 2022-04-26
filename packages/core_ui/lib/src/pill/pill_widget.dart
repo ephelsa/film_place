8ac43dac82ff5e088131a3f69716da5b611f4e95
Leonardo <xephelsax@gmail.com>
2022-04-26 10:10:00 -0500

    Improve `PillWidget` layout

--- packages/core_ui/lib/src/pill/pill_widget.dart
+++ packages/core_ui/lib/src/pill/pill_widget.dart
@@ -9,23 +9,28 @@ class PillWidget extends StatelessWidget {
   }) : super(key: key);
 
   final Color? color;
-  final Alignment alignment;
+  final Alignment? alignment;
   final Widget? child;
 
   @override
   Widget build(BuildContext context) {
     final _color = color ?? Theme.of(context).colorScheme.primary;
-
-    return Align(
-      alignment: alignment,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(8),
-        child: Container(
-          color: _color,
-          padding: const EdgeInsets.all(8),
-          child: child,
-        ),
+    final content = ClipRRect(
+      borderRadius: BorderRadius.circular(8),
+      child: Container(
+        color: _color,
+        padding: const EdgeInsets.all(8),
+        child: child,
       ),
     );
+
+    if (alignment != null) {
+      return Align(
+        alignment: alignment!,
+        child: content,
+      );
+    }
+
+    return content;
   }
 }
